d3b01c63797f3cf8c6fe52109da6f710f0794621
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-26 09:59:36 -0600

    ON page load run IMDB api function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-$(function() {
-  'use strict';
-  console.log('sanity check');
+$(function(){
+  $('form').on('submit', function(event){
+    event.preventDefault();
+   var id = $('#omdb').val() ;
+   console.log(id);
+   ajaxCall(id);
+    //  $('#output').append('');
+  });
+  ajaxCall('grease');  
 });
+
+
+function ajaxCall(id){
+  $.ajax({
+    method: 'GET',
+    url: 'http://www.omdbapi.com/?t=' + id
+  }).done(function(OMDBresults){
+    console.log(OMDBresults);
+  });
+}
